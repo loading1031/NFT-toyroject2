48b5b0e6232b81a4b735491da9999f02cf6a8ad9
loading1031 <tjdans1031@gmail.com>
2025-01-28 05:46:57 +0900

    feat: create HelpCenter component

--- NFT-template/src/components/NavBar/HelpCenter/HelpCenter.tsx
+++ NFT-template/src/components/NavBar/HelpCenter/HelpCenter.tsx
@@ -1,8 +1,37 @@
+import { Link } from "react-router-dom";
+
+//INTERNAL IMPORT
 import Style from './HelpCenter.module.css'
 
 const HelpCenter = () => {
+    const helpCenter = [
+        {
+            name: "About",
+            link: "about"
+        },
+        {
+            name: "Contact Us",
+            link: "contact-us"
+        },
+        {
+            name: "Sign Up",
+            link: "sign-up"
+        },
+        {
+            name: "Subscription",
+            link: "subscription"
+        }
+    ]
     return (
-        <div>HelpCenter</div>
+        <div className={Style.box}>
+            {
+                helpCenter.map((el, i) => (
+                    <div key={i + 1} className={Style.helpCenter}>
+                        <Link to={`/${el.link}`}>{el.name}</Link>
+                    </div>
+                ))
+            }
+        </div>
     )
 }
 
