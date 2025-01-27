d703316321e86841f84ea3255c93d2ecba46fe4a
loading1031 <tjdans1031@gmail.com>
2025-01-28 06:08:40 +0900

    feat: create notification component

--- NFT-template/src/components/NavBar/Notification/Notification.tsx
+++ NFT-template/src/components/NavBar/Notification/Notification.tsx
@@ -1,8 +1,30 @@
+import { Img } from "react-image"
+
+//INTERNAL IMPORT
 import Style from './Notification.module.css'
+import images from '../../../img'
+
 
 const Notification = () => {
     return (
-        <div>Notification</div>
+        <div className={Style.Notification}>
+            <p>Notification</p>
+            <div className={Style.notification_box}>
+                <div className={Style.notification_box_img}>
+                    <Img 
+                        src={images.user1} alt="profile image" 
+                        width={50} height={50} 
+                        className={Style.notification_box_img} 
+                    />
+                </div>
+                <div className={Style.notification_box_info}>
+                    <h4>Shoaib Akhter</h4>
+                    <p>Measure action your user...</p>
+                    <small>3 minutes ago</small>
+                </div>
+                <span className={Style.notification_box_new}></span>
+            </div>
+        </div>
     )
 }
 
