5a916e9d4eef26ada4205c4772857f1dfa27e383
loading1031 <tjdans1031@gmail.com>
2025-01-28 00:28:40 +0900

    config: use amoy test net

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,34 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.28",
+  solidity: {
+    version: "0.8.28",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  networks: {
+    amoy: {
+      url: process.env.REACT_APP_ALCHEMY_API_URL,
+      accounts: [process.env.REACT_APP_PRIVATE_KEY]
+    },
+  },
 };
+
+/*
+enabled: true, runs: 200:
+
+대부분의 스마트 계약에서 권장되는 기본 설정.
+배포 및 실행 가스 비용의 균형을 맞추는 적절한 값.
+enabled: true, runs: 1:
+
+테스트 목적으로 배포 가스 비용을 최소화해야 할 때 유용.
+enabled: false:
+
+스마트 계약 디버깅이나 간단한 테스트 실행에 적합.
+*/
