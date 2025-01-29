af6494bec39633d6430653bde34ea425633836b9
loading1031 <tjdans1031@gmail.com>
2025-01-29 16:27:27 +0900

    feat: create service component

--- NFT-template/src/components/components-index.ts
+++ NFT-template/src/components/components-index.ts
@@ -2,5 +2,6 @@ import Button from './Button/Button';
 import Footer from './Footer/Footer';
 import HeroSection from './HeroSection/HeroSection';
 import NavBar from './NavBar/NavBar';
+import Service from './Service/Service';
 
-export { Button, Footer, NavBar, HeroSection }
+export { Button, Footer, NavBar, HeroSection, Service }
--- NFT-template/src/pages/index.tsx
+++ NFT-template/src/pages/index.tsx
@@ -1,11 +1,12 @@
 //INTERNAL IMPORT
 import Style from '../styles/index.module.css'
-import { HeroSection } from '../components/components-index'
+import { HeroSection, Service } from '../components/components-index'
 
 const Home = () => {
     return (
         <div className={Style.homePage}>
             <HeroSection />
+            <Service />
         </div>
     )
 }
